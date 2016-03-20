f302773fb7f48a476e09e6e149b94fb86534c440
Matt Tarabulski <bulski7@gmail.com>
2016-03-20 10:31:39 -0400

    added a forward kinematics test

--- ScaraSim.py
+++ ScaraSim.py
@@ -10,7 +10,14 @@ from scara5 import FiveBar
 scara = FiveBar();
 scara.L = [2.5,2,2,2,2];
 
+#Forward Kinematics Test
+for th1 in np.linspace(0,2*math.pi,50):
+    for th4 in np.linspace(0,2*math.pi,50):
+        scara.SetDriveArmPositions(th1,th4);
+        if(not scara.DriveArmsIntersect()):
+            scara.ShowPosture();
 
+#Inverse Kinematics Test
 #Move the end effector at CV
 for x in np.linspace(-2,4.5,50):
     for y in np.linspace(-4,4,50):
